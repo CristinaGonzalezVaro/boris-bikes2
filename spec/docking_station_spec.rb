0a90b63992705fe1c75ab18d37a8bce9aad55495
Cristina <cristhinagv@gmail.com>
2022-01-19 18:40:37 +0000

    added new raised error when docking station is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,10 +15,20 @@ describe DockingStation do
     end
   end
 
-  it 'docks a bike' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+  describe '#dock' do
+    it 'docks a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
   end
+  
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
   
   it 'returns docked bikes' do
     bike = Bike.new
